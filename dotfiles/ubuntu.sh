cac2861c62a6e214b59026e3e939b28de8cd87fb
Francisco Solis <imfran@duck.com>
2023-07-19 19:06:25 -0400

    Add brew packages

--- dotfiles/ubuntu.sh
+++ dotfiles/ubuntu.sh
@@ -10,8 +10,7 @@ sudo apt install -y build-essential procps curl file git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install brew packages from github required for the development.
-BREW_PACKAGES=$()
-brew install "$BREW_PACKAGES"
+brew install "$(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/brew-packages)"
 
 # Install oh my zsh.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
