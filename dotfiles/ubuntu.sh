c02dbf938d8282454a61ff2e9d139672101dc545
Francisco Solis <imfran@duck.com>
2023-07-19 22:21:46 -0400

    Update ubuntu.sh

--- dotfiles/ubuntu.sh
+++ dotfiles/ubuntu.sh
@@ -4,22 +4,22 @@
 sudo apt update
 
 # Install required packages
-sudo apt install -y build-essential procps curl file git
+sudo apt install -y build-essential procps curl file git zsh keychain
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Move the current ~/.zshrc file to a backup if it exists (to ~/.zshrc.bak)
-[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc-$(date +%Y%m%d).bak
-
-# Generate a new zshrc file
-(echo '# Start of .zshrcfile'; echo; echo '# Init brew';echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.zshrc
-
-# Load current zshrc file
-source ~/.zshrc
+# Load homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install brew packages from github required for the development.
 brew install $(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/brew-packages)
 
 # Install oh my zsh.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Move the current ~/.zshrc file to a backup if it exists (to ~/.zshrc.bak)
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc-$(date +%Y%m%d).bak
+
+# Generate new ~/.zshrc file
+curl -o ~/.zshrc https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/ubuntu/zshrc
