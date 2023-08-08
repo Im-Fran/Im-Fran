88529e307686cda8d1886fea873a3a41da24c41a
Francisco Solis <imfran@duck.com>
2023-08-08 19:42:40 -0400

    Update packages.sh

--- dotfiles/brew/packages.sh
+++ dotfiles/brew/packages.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Install homebrew packages
-brew install zsh gh swift stripe/stripe-cli/stripe
+# Install homebrew global packages
+brew install gh stripe/stripe-cli/stripe
+
+# Install non macos packages (any linux distro)
+if [ "$(uname)" != "Darwin" *; then
+  brew install zsh swift
+fi
