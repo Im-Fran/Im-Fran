200c372ab262685b78a8eaf5d9f72b9f2a750eb4
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2026-06-06 00:30:32 -0400

    fix: update linux.sh for improved package installation and paths; add .gitignore for DS_Store

--- dotfiles/linux.sh
+++ dotfiles/linux.sh
@@ -1,10 +1,10 @@
-#/bin/sh
+#!/bin/sh
 
 # Request sudo access (and update apt)
 sudo apt update
 
 # Install required packages
-sudo apt install -y build-essential procps curl file git zsh keychain
+sudo apt install -y build-essential procps curl file git zsh keychain jq
 
 # Install brew
 if ! command -v brew &> /dev/null; then
@@ -14,31 +14,31 @@ if ! command -v brew &> /dev/null; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-# Install brew taps from github required for the development.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/brew/taps.sh)"
+# Install brew taps
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/shared/brew/taps.sh)"
 
-# Install brew packages from github required for the development.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/brew/packages.sh)"
+# Install brew packages
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/shared/brew/packages.sh)"
 
-# Install oh my zsh.
+# Install oh my zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
   RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   # Install theme
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
-# Create scripts folder and symlink
-[ ! -d $HOME/.scripts ] &&mkdir /Volumes/Development/.scripts && ln -s /Volumes/Development/.scripts $HOME/.scripts
+# Create scripts folder
+[ ! -d $HOME/.scripts ] && mkdir $HOME/.scripts
 
 # Setup swift autocompletion
-swift package completion-tool generate-bash-script > ~/.scripts/swift-completion-tool
-chmod +x ~/.scripts/swift-completion-tool
+swift package completion-tool generate-bash-script > $HOME/.scripts/swift-completion-tool
+chmod +x $HOME/.scripts/swift-completion-tool
 
-# Move the current ~/.zshrc file to a backup if it exists (to ~/.zshrc.bak)
+# Move the current ~/.zshrc file to a backup if it exists
 [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc-$(date +%Y%m%d).bak
 
 # Generate new ~/.zshrc file
-curl -o ~/.zshrc https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/ubuntu/zshrc
+curl -o ~/.zshrc https://raw.githubusercontent.com/Im-Fran/Im-Fran/main/dotfiles/platforms/linux/zshrc
 
 # Load current zshrc
 source ~/.zshrc
--- dotfiles/shared/brew/packages.sh
+++ dotfiles/shared/brew/packages.sh
@@ -4,6 +4,6 @@
 brew install gh stripe/stripe-cli/stripe gradle
 
 # Install non macos packages (any linux distro)
-if [ "$(uname)" != "Darwin" *; then
+if [ "$(uname)" != "Darwin" ]; then
   brew install zsh swift
 fi
